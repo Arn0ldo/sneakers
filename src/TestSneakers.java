1469688d8ae5a44db72a9407a5bc4fca33483440
Arn0ldo <21670177@iguala.tecnm.mx>
2023-03-20 21:46:26 -0600

    primer commit

---
+++ src/TestSneakers.java
@@ -0,0 +1,17 @@
+public class TestSneakers {
+    public static void main(String[] args) {
+    }
+
+    public class TheSneakers {
+        public static void main(String[] args) {
+            Sneakers tenis = new Sneakers();
+            tenis.setModel(2023);
+            tenis.setSize(8.5);
+            tenis.setColor("negros");
+            tenis.setMarca("Pumas");
+
+
+            tenis.toString();
+        }
+    }
+}
